a4675adf1686584cc81bfa44821ea7902b8ed14f
MfenaKaase <68476762+MfenaKaase@users.noreply.github.com>
2024-12-11 07:52:18 +0100

    Add API authentication for user login

---
+++ backend/app/Http/Controllers/LoginController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $user = User::where('email', $request['email'])->firstOrFail();
+            $token = $user->createToken('auth_token')->plainTextToken;
+        
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ]);
+        }
+ 
+        return response()->json([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
+    
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,14 +4,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ChildController;
+use App\Http\Controllers\LoginController;
 
+//  Auth Route
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/authentication', [LoginController::class, 'authenticate']);
+
 
 // Users
 Route::resource('users', UserController::class);
 
 // Children
 Route::resource('children', ChildController::class);
+
+
 
